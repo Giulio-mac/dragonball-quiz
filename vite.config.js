a6d1d15befde8ed1ea88caceda695d5539d5fc97
Giulio-mac <giulio.mac2006@gmail.com>
2024-10-28 14:21:03 +0100

    modifiche

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-node'; // Cambia l'adattatore a Node
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    kit: {
+        adapter: adapter(), // Usa l'adattatore Node.js
+        paths: {
+            base: '/dragonball-quiz', // Mantieni il nome della tua repository
+        },
+        prerender: {
+            default: true // Questo può rimanere se vuoi prerenderizzare tutto
+        }
+    }
 });
