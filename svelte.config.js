1a2faa51829e98e853467b2f959a0c365c1c3fcb
Giulio-mac <giulio.mac2006@gmail.com>
2024-10-29 13:45:15 +0100

    the last dance

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-import adapter from '@sveltejs/adapter-node'; // Importa l'adattatore per Node.js
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter(), // Configura l'adapter Node.js
-        paths: {
-            base: '/dragonball-quiz', // Usa il nome della tua repository
-        }
-    }
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
 
 export default config;
